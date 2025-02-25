a23cc0c089802c209c081023bb75f54c7c4f44e6
Abdo-Nabil <93052392+Abdo-Nabil@users.noreply.github.com>
2025-02-25 15:56:57 +0200

    remove unUsed reactions

--- lib/reaction_data.dart
+++ lib/reaction_data.dart
@@ -2,10 +2,10 @@ class ReactionData {
   static List<String> facebookReactionIcon = [
     "assets/images/like.gif",
     "assets/images/love.gif",
-    "assets/images/care.gif",
-    "assets/images/haha.gif",
-    "assets/images/wow.gif",
     "assets/images/sad.gif",
-    "assets/images/angry.gif"
+    "assets/images/angry.gif",
+    "assets/images/haha.gif",
+    // "assets/images/care.gif",
+    // "assets/images/wow.gif",
   ];
 }
